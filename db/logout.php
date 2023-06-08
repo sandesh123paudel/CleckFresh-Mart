86f1d96595621b80064c1479ac729457b96ce844
Karan Chaudhary <tharukaran930@gmail.com>
2023-06-08 08:58:54 +0545

    update

--- db/logout.php
+++ db/logout.php
@@ -1,35 +1,36 @@
 <?php
-    session_start();
-    include('connection.php');
+session_start();
+include('connection.php');
 
-    $status = 'off';    
-    $sql = "UPDATE USER_I SET STATUS = :active WHERE USER_ID= :id";
-    $stid = oci_parse($connection,$sql);
+$status = 'off';
+$sql = "UPDATE USER_I SET STATUS = :active WHERE USER_ID= :id";
+$stid = oci_parse($connection, $sql);
 
-    oci_bind_by_name($stid,':active' ,$status);
+oci_bind_by_name($stid, ':active', $status);
 
-    if(isset($_SESSION['userID'])){
-        oci_bind_by_name($stid, ':id' , $_SESSION['userID']);
-      }
-    if(isset($_SESSION['traderID'])){
-        oci_bind_by_name($stid, ':id' , $_SESSION['traderID'] );
-      }
-    if(isset($_SESSION['adminID'] )){
-      oci_bind_by_name($stid, ':id' , $_SESSION['adminID'] );
-    }
-      
-    oci_execute($stid);
+if (isset($_GET['role'])) {
 
-    if($_GET['role']== 'customer'){
-      unset($_SESSION['userID']);
-      unset($_SESSION['token']);
-      unset($_SESSION['cart_id']);
-    }
-    if($_GET['role']== 'trader'){
-      unset($_SESSION['traderID']);
-    }
-    if($_GET['role']== 'admin'){
-      unset($_SESSION['adminID']);
-    }
 
-    header('location:../login.php');
+  if ($_GET['role'] == 'customer') {
+    oci_bind_by_name($stid, ':id', $_SESSION['userID']);
+
+    unset($_SESSION['userID']);
+    unset($_SESSION['token']);
+    unset($_SESSION['cart_id']);
+  }
+  if ($_GET['role'] == 'trader') {
+    oci_bind_by_name($stid, ':id', $_SESSION['traderID']);
+
+    unset($_SESSION['traderID']);
+  }
+  if ($_GET['role'] == 'admin') {
+    oci_bind_by_name($stid, ':id', $_SESSION['adminID']);
+    unset($_SESSION['adminID']);
+  }
+
+  if (oci_execute($stid)) {
+    echo "success";
+  }
+
+  header('location:../login.php');
+}
